ae2edc5270eaac28b4b31654f14123c8b1b58eab
arun0808rana <arun0808rana@gmail.com>
2023-05-10 19:10:48 +0530

    add: home carousels

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,35 +4,46 @@ import useFetchHome from "./useFetchHome";
 
 function Home() {
   const [favourites, playlists, recentlyAdded, recentlyPlayed] = useFetchHome();
-
+  console.log('favourites', favourites)
   return (
     <div className="home">
-      <h2>Favourites</h2>
       <div className="favourites">
-        {favourites.slice(0, 5).map((track) => {
-          return <Track track_name={track} key={`track_${track.id}`} />;
-        })}
+        <h3 className="home_headings">Favourites</h3>
+        <div className="favourites__card_container">
+          {favourites.slice(0, 5).map((track) => {
+            return <Track track={track} key={`track_card_id_${track.id}`} />;
+          })}
+        </div>
       </div>
 
-      <h2>Playlists</h2>
+
       <div className="playlists">
-        {playlists.slice(0, 5).map((track) => {
-          return <Track track_name={track} key={`track_${track.id}`} />;
-        })}
+        <h3 className="home_headings">Playlists</h3>
+        <div className="playlists__card_container">
+          {playlists.slice(0, 5).map((track) => {
+            return <Track track={track} key={`track_card_id_${track.id}`} />;
+          })}
+        </div>
       </div>
 
-      <h2>Recently Added</h2>
+
       <div className="recently_added">
-        {favourites.slice(0, 5).map((track) => {
-          return <Track track_name={track} key={`track_${track.id}`} />;
-        })}
+        <h3 className="home_headings">Recently Added</h3>
+        <div className="recently_added__card_container">
+          {favourites.slice(0, 5).map((track) => {
+            return <Track track={track} key={`track_card_id_${track.id}`} />;
+          })}
+        </div>
       </div>
 
-      <h2>Recently Played</h2>
+
       <div className="recently_played">
-        {favourites.slice(0, 5).map((track) => {
-          return <Track track_name={track} key={`track_${track.id}`} />;
-        })}
+        <h3 className="home_headings">Recently Played</h3>
+        <div className="recently_played__card_container">
+          {favourites.slice(0, 5).map((track) => {
+            return <Track track={track} key={`track_card_id_${track.id}`} />;
+          })}
+        </div>
       </div>
     </div>
   );
@@ -40,12 +51,24 @@ function Home() {
 
 export default Home;
 
-export function Track({ coverArtURL, artist, track_name }) {
+export function Track({ track }) {
+  const { thumbnail_url: coverArtURL, artist, track_name } = track;
   return (
     <div className="track_card">
-      <img src="" className="track_card__cover_art" />
-      <div className="track_card__artist_name"></div>
-      <div className="track_card__track_name"></div>
+      <div className="desktop_track_card">
+        <img src={coverArtURL} className="track_card__cover_art" />
+        <div className="track_card_details">
+          <div className="track_card__track_name">{track_name.substring(0, 20)}</div>
+          <div className="track_card__artist_name">{artist}</div>
+        </div>
+      </div>
+      <div className="mobile_track_card">
+        <img src={coverArtURL} className="mobile_track_card__track_card__cover_art" />
+        <div className="mobile_track_card__track_card_details">
+          <div className="mobile_track_card__track_card__track_name">{track_name.substring(0, 20)}</div>
+          <div className="mobile_track_card__track_card__artist_name">{artist}</div>
+        </div>
+      </div>
     </div>
   );
 }
